0cea268091c075b7e33758f5b7af6a429e7f302a
jycouet <jycouet@gmail.com>
2023-12-06 14:11:23 +0100

    add @vite-pwa/sveltekit and manifest conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,40 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				short_name: 'PWA SvelteKit',
+				name: 'PWA SvelteKit',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
+					}
+				]
+			}
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
