6760463f32c1e591ce56ffa0faa2e5703227a345
userquin <userquin@gmail.com>
2023-12-06 17:37:17 +0100

    feat: add dynamic country pages configuration

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -8,8 +8,21 @@ import {
 	precacheAndRoute
 } from 'workbox-precaching';
 import { NavigationRoute, registerRoute } from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 declare let self: ServiceWorkerGlobalScope;
+
+const countryPagesCache = 'countries-pages';
+
+self.addEventListener('install', (event) => {
+	// TODO: replace log with empty function
+	event.waitUntil(
+		self.caches
+			.open(countryPagesCache)
+			.then(() => console.info(`Opened cache: ${countryPagesCache}`))
+	);
+});
 
 self.addEventListener('message', (event) => {
 	if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
@@ -21,8 +34,27 @@ precacheAndRoute(self.__WB_MANIFEST);
 // clean old assets
 cleanupOutdatedCaches();
 
+const countriesRegex = /^\/countries\/.*/i;
+const denylist = [countriesRegex];
 let allowlist: undefined | RegExp[];
 if (import.meta.env.DEV) allowlist = [/^\/$/];
 
+registerRoute(
+	({ sameOrigin, url }) => sameOrigin && url.pathname.match(countriesRegex),
+	new NetworkFirst({
+		cacheName: countryPagesCache,
+		matchOptions: {
+			ignoreVary: true,
+			ignoreSearch: true
+		},
+		plugins: [
+			{
+				// TODO: we can add custom logic here adding some params to the response, then hanlde it on the custom page
+				handlerDidError: async () => Response.redirect('/error?offline', 302)
+			},
+			new CacheableResponsePlugin({ statuses: [200] })
+		]
+	})
+);
 // to allow work offline
-registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist }));
+registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist, denylist }));
