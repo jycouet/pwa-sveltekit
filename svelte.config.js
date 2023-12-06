300f6ebe8e44cc8a7d30d501b9cf5d9c382ae0ed
jycouet <jycouet@gmail.com>
2023-12-06 15:36:54 +0100

    add fallback

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			strict: false
+			fallback: '200.html' // may differ from host to host
+			// strict: false
 		})
 	}
 };
